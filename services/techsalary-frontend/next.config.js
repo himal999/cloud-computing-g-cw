1726434a2d286914842b95351d22877be94c0e72
Sasfak-Ahamed <ssasfak@gmail.com>
2026-04-25 02:17:39 +0530

    feat: initial Azure Kubernetes deployment setup

--- services/techsalary-frontend/next.config.js
+++ services/techsalary-frontend/next.config.js
@@ -1,7 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   env: {
-    NEXT_PUBLIC_BFF_URL: process.env.NEXT_PUBLIC_BFF_URL || 'http://localhost:5000',
+    NEXT_PUBLIC_BFF_URL: 'http://4.188.102.12',
   },
 }
 module.exports = nextConfig
